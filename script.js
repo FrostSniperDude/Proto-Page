a4577db0bae21f2bef2d97cf6b2b264911b8100e
frost <snipe@DESKTOP-1TRPBSU>
2025-06-27 12:32:15 -0500

    Inital Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://mjtvoolokpjzqrxjzmti.supabase.co/functions/v1/database-access', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+            alert(JSON.parse(xhr.responseText).message)
+        } else {
+            alert(JSON.parse(xhr.responseText).error)
+        }
+    }
+    };
+
+    const data = JSON.stringify({
+    username: username,
+    password: password
+    });
+
+    xhr.send(data);  // Send the request with the data
+});
