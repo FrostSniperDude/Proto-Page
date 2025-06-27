d5451761a9982978f66302a17913c7853655b692
frost <snipe@DESKTOP-1TRPBSU>
2025-06-27 13:27:32 -0500

    Added tiemr delay

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
+var lastRequest = new Date().getTime()
+const delay = 1000
+
 document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    if (lastRequest - new Date().getTime() > delay) {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://mjtvoolokpjzqrxjzmti.supabase.co/functions/v1/database-access', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://mjtvoolokpjzqrxjzmti.supabase.co/functions/v1/database-access', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            alert(JSON.parse(xhr.responseText).message)
-        } else {
-            alert(JSON.parse(xhr.responseText).error)
+        xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert(JSON.parse(xhr.responseText).message)
+            } else {
+                alert(JSON.parse(xhr.responseText).error)
+            }
         }
+        };
+
+        const data = JSON.stringify({
+        requestType: "signup",
+        username: username,
+        password: password
+        });
+
+        xhr.send(data);  // Send the request with the data
     }
-    };
-
-    const data = JSON.stringify({
-    requestType: "signup",
-    username: username,
-    password: password
-    });
-
-    xhr.send(data);  // Send the request with the data
+    else {
+        alert("Please slow your requests")
+    }
 });
